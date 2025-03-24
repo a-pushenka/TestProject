1a4e55fcca830c6f58002fda3e6ec5aefadb26b2
Anna.Pushenka <osipenyshka@gmail.com>
2025-03-24 10:29:54 +0300

    message "Hi there" is changed to "Hello World"

--- src/main/java/course/section3/Test.java
+++ src/main/java/course/section3/Test.java
@@ -2,6 +2,11 @@ package course.section3;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hi there");
+        System.out.println("Hello World");
+        //comment
+        /*
+        comment 2
+        ha-ha
+         */
     }
 }

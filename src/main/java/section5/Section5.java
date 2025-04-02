2fdc553449b829218cf200178e8c823bdba03fdc
Anna.Pushenka <osipenyshka@gmail.com>
2025-04-02 15:24:54 +0300

    message "Hi there" is changed to "Hello World"

---
+++ src/main/java/section5/Section5.java
@@ -0,0 +1,56 @@
+package section5;
+
+public class Section5 {
+
+//45-47.
+
+//    public static void main(String[] args) {
+//
+//        calculatedScore (true, 800, 5, 100);
+//
+//        calculatedScore(true, 10000, 8, 200);
+//
+//    }
+//
+//    public static void calculatedScore (boolean gameOver, int score, int levelCompleted, int bonus) {
+//// void means that no data will be returned from the method
+//
+//        int finalScore = score;
+//
+//        if (gameOver) {
+//            finalScore += (levelCompleted * bonus);
+//            finalScore += 1000;
+//            System.out.println("45-47 lessons. Your final score was " + finalScore);
+//        }
+//    }
+
+    // 47.
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore = calculatedScore (gameOver, score, levelCompleted, bonus);
+        System.out.println("The highScore is " + highScore);
+
+        calculatedScore(true, 10000, 8, 200);
+
+    }
+
+    public static int calculatedScore (boolean gameOver, int score, int levelCompleted, int bonus) {
+// int means that no data will be returned from the method
+
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+        }
+
+        return finalScore;
+        // return statement
+    }
+
+}

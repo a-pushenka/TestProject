73d7215f1f027b3fcb7e4b68bf980cbcf95ae966
Anna.Pushenka <osipenyshka@gmail.com>
2025-04-03 11:40:09 +0300

    message "Hi there" is changed to "Hello World"

---
+++ src/main/java/section5/Excercise1.java
@@ -0,0 +1,12 @@
+package section5;
+
+public class Excercise1 {
+
+
+        // public class PositiveNegativeZero exercise1
+
+    public static void checkNumber (int number) {
+
+        System.out.println((number > 0) ? "positive" : ((number < 0) ? "negative" : "zero"));
+    }
+}
--- src/main/java/section5/Section5.java
+++ src/main/java/section5/Section5.java
@@ -88,13 +88,15 @@ public class Section5 {
 
     public static int calculateHighScorePosition(int playerScore) {
 
-        int position = 4;
+        int position = 0;
         if (playerScore >= 1000) {
             position = 1;
         } else if (playerScore >= 500) {  // && playerScore < 1000 there is no need to add it
             position = 2;
         } else if (playerScore >= 100) {  //&& playerScore < 500
             position = 3;
+        } else {
+            position = 4;
         }
 
         return position;

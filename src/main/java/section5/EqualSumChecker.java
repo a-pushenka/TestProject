3b3ae9ce8969054e41054f58ed6c779da3e6902b
Anna.Pushenka <osipenyshka@gmail.com>
2025-04-07 12:47:39 +0300

    message "Hi there" is changed to "Hello World"

--- src/main/java/section5/EqualSumChecker.java
+++ src/main/java/section5/EqualSumChecker.java
@@ -1,4 +1,20 @@
 package section5;
 
 public class EqualSumChecker {
+    public static void main(String[] args) {
+        boolean comparison = hasEqualSum(1, -1, 0);
+        System.out.println(comparison);
+    }
+    public static boolean hasEqualSum (int first, int second, int third) {
+        return (first + second) == third;
+    }
 }
+
+/*
+int sumOfFirstAndSecond = first + second;
+        return sumOfFirstAndSecond == third;
+ */
+/*
+boolean ddd = ((a +b) == c);
+return ddd;
+ */

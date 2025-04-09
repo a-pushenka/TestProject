45710d7802a95b88289f232eb53b7d1fdad76a96
Anna.Pushenka <osipenyshka@gmail.com>
2025-04-09 19:16:17 +0300

    message "Hi there" is changed to "Hello World"

--- src/main/java/section5/PlayingCat.java
+++ src/main/java/section5/PlayingCat.java
@@ -1,4 +1,27 @@
 package section5;
 
 public class PlayingCat {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+
+    }
+    public static boolean isCatPlaying (boolean summer, int temperature) {
+        if (!summer && temperature >= 25 && temperature <= 35) {
+            return true;
+        } else if (summer && temperature >= 25 && temperature <= 45) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+/*
+public class PlayingCat {
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+
+        int max = summer ? 45 : 35;
+        return temperature >= 25 && temperature <= max;
+    }
+}
+ */

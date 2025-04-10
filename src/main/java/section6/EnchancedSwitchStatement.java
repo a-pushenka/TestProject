dd731bd8be4c9e4ceaa551ccb2b17b087e49cfdf
Anna.Pushenka <osipenyshka@gmail.com>
2025-04-10 12:29:46 +0300

    message "Hi there" is changed to "Hello World"

--- src/main/java/section6/EnchancedSwitchStatement.java
+++ src/main/java/section6/EnchancedSwitchStatement.java
@@ -4,6 +4,27 @@ public class EnchancedSwitchStatement {
     public static void main(String[] args) {
         int switchValue = 3;
         switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Was a 3, or 4, or 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+            default :
+                System.out.println("Was not 1, 2, 3, 4, or 5");
+                break;
+        }
+    }
+}
+
+/*
+public static void main(String[] args) {
+        int switchValue = 3;
+        switch (switchValue) {
             case 1 -> System.out.println("Value was 1");
             case 2 -> System.out.println("Value was 2");
             case 3, 4, 5 -> {
@@ -13,4 +34,4 @@ public class EnchancedSwitchStatement {
             default -> System.out.println("Was not 1, 2, 3, 4, or 5");
         }
     }
-}
+ */

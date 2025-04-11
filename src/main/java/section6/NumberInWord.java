1f5ac087aab43ff1b3e47a1fb6b91027463ca188
Anna.Pushenka <osipenyshka@gmail.com>
2025-04-11 13:57:33 +0300

    message "Hi there" is changed to "Hello World"

---
+++ src/main/java/section6/NumberInWord.java
@@ -0,0 +1,27 @@
+package section6;
+
+public class NumberInWord {
+    public static void main(String[] args) {
+        printNumberInWord(0);
+        printNumberInWord(1);
+        printNumberInWord(-1);
+
+    }
+
+    public static void printNumberInWord (int number) {
+        String numbInWord = switch (number) {
+            case 0 -> "ZERO";  // {yield "ZERO";} because of void?
+            case 1 -> "ONE";
+            case 2 -> "TWO";
+            case 3 -> "THREE";
+            case 4 -> "FOUR";
+            case 5 -> "FIVE ";
+            case 6 -> "SIX";
+            case 7 -> "SEVEN";
+            case 8 -> "EIGHT";
+            case 9 -> "NINE";
+            default -> "OTHER";
+        };
+        System.out.println(number + " = " + numbInWord);
+    }
+}

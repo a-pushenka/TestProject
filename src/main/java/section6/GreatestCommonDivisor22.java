9b5af3b96ebb4a4a4db9fbed8134fa5fe1743fb8
Anna.Pushenka <osipenyshka@gmail.com>
2025-05-06 12:25:31 +0300

    message "Hi there" is changed to "Hello World"

--- src/main/java/section6/GreatestCommonDivisor22.java
+++ src/main/java/section6/GreatestCommonDivisor22.java
@@ -2,24 +2,26 @@ package section6;
 
 public class GreatestCommonDivisor22 {
     public static void main(String[] args) {
-        getGreatestCommonDivisor(25, 15);
+        getGreatestCommonDivisor(10, 15);
 
     }
 
     public static int getGreatestCommonDivisor(int first, int second) {
-        if (first < 0 || second < 0) {
-            System.out.println("Invalid Value: value first or second is less than 0.");
+        if (first < 10 || second < 10) {
+            System.out.println("Invalid Value: value first or second is less than 10.");
             return -1;
         }
 
+        System.out.println("");
         System.out.print("Divisors for the first number ");
         for (int i = 1; i <= first; i++) {
-            if ((first % i) == 0) {
+            if (first % i == 0) {
                 System.out.print(i + " ");
                 continue;
             }
         }
-        System.out.println(" ");
+
+        System.out.println("");
         System.out.print("Divisors for the second number ");
         for (int a = 1; a <= second; a++) {
             if ((second % a) == 0) {
@@ -28,6 +30,16 @@ public class GreatestCommonDivisor22 {
             }
         }
 
-        return 1;
+        int min = first < second ? first : second;
+        int gcd = 1;
+        for (int j = 1; j <= min; j++) {
+            if (first % j == 0 && second % j == 0) {
+                gcd = j;
+            }
+        }
+
+        System.out.println("");
+        System.out.println("Greatest common divisor is " + gcd);
+        return gcd;
     }
 }

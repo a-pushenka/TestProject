729f2439fd3755eec3c32f038b0b8a33f8e8a82c
Anna.Pushenka <osipenyshka@gmail.com>
2025-08-01 18:08:14 +0300

    message "Hi there" is changed to "Hello World"

--- src/main/java/section6/Scaner74Challenge.java
+++ src/main/java/section6/Scaner74Challenge.java
@@ -10,17 +10,19 @@ public class Scaner74Challenge {
         int counter = 1;
         int sum = 0;
 
-        while (counter <= 5) {
+        while (counter <= 5) // do можно вместо while (counter <=5)
+        {
             System.out.println("Enter number #" + counter + ":");
             String nextNumber = scanner.nextLine();
             try {
                 int number = Integer.parseInt(nextNumber);
+                            // double number = Double.parseDouble(nextNumber);
                 counter++;
                 sum += number;
             } catch (NumberFormatException nfe) {
                 System.out.println("Invalid number");
             }
-        }
+        }  // если написать do то тут нужно написать while (counter <= 5);
 
         System.out.println("The sum of the 5 number = " + sum);
     }

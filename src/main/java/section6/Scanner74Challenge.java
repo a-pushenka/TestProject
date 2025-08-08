1e298f74071155dbace2dad6ad7d4f2d2b518be1
Anna.Pushenka <osipenyshka@gmail.com>
2025-08-08 17:18:41 +0300

    message "Hi there" is changed to "Hello World"

--- src/main/java/section6/Scanner74Challenge.java
+++ src/main/java/section6/Scanner74Challenge.java
@@ -2,16 +2,14 @@ package section6;
 
 import java.util.Scanner;
 
-public class Scaner74Challenge {
-
+public class Scanner74Challenge {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int counter = 1;
         int sum = 0;
 
-        while (counter <= 5) // do можно вместо while (counter <=5)
-        {
+        while (counter <= 5) { // do можно вместо while (counter <=5)
             System.out.println("Enter number #" + counter + ":");
             String nextNumber = scanner.nextLine();
             try {
